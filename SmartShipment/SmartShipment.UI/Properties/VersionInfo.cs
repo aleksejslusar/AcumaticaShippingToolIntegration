19e3bd8574803f92678f1a61bb3c3d6aec513dbb
Alexey Slusar <aleksej.slusar@sprinterra.com>
2018-01-15 13:57:22 +0200

    Fix WiX Setup post-build action

--- SmartShipment/SmartShipment.UI/Properties/VersionInfo.cs
+++ SmartShipment/SmartShipment.UI/Properties/VersionInfo.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("1.0.262.26")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.262.26")]
+[assembly: System.Reflection.AssemblyVersion("1.0.290.4")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.290.4")]
 
 
